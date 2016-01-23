3f320864cbde4936c4bbc6feaa508d2667a1d28f
Mike Lowen <mike@mlowen.com>
2016-01-23 15:13:20 +1300

    Porting build file to Gulp.
    Moved the functionality for running the tests into a gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var jasmine = require('gulp-jasmine-phantom');
 
-gulp.task('build:source', function() {
+var jsFiles = [
+	'./build/url-query-parser.js',
+	'./build/route.js',
+	'./build/router.js',
+	'./build/extension.js'
+]
+
+gulp.task('build:source', function () {
 	return gulp.src('./src/**/*.coffee')
 		.pipe(coffee({ bare: true }))
 		.pipe(gulp.dest('./build'));
+});
+
+gulp.task('build:tests', function () {
+	return gulp.src('./tests/**/*.coffee')
+		.pipe(coffee({ bare: true }))
+		.pipe(gulp.dest('./tests'));
+});
+
+gulp.task('run:tests', [ 'build' ], function () {
+	return gulp.src('./tests/**/*.js')
+		.pipe(jasmine({
+			jasmineVersion: "2.3",
+			integration: true,
+			vendor: [
+				'./node_modules/knockout/build/output/knockout-latest.debug.js',
+				'./build/**/*.js'
+			]
+		}));
 });
 
 gulp.task('build', [ 'build:source' ]);
