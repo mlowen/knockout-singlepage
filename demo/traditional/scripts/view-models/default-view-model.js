038482dd3d2987b8bf5eb1e7aad20dd36ff72ee1
Mike Lowen <mlowen@lic.co.nz>
2016-02-08 14:45:04 +1300

    Porting to javascript.
    Moved the traditional demo to JS.

---
+++ demo/traditional/scripts/view-models/default-view-model.js
@@ -0,0 +1,7 @@
+var DefaultViewModel = function () {
+	var self = this;
+	
+	self.input = ko.observable(null);
+	self.valid = ko.computed(function () { return self.input() != null && self.input() > 0; });
+	self.onClick = function () { console.log('Element was clicked.'); };
+};
